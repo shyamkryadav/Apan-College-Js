bc7409e137cc25ec4c3f54c17be3ed07a77d150a
shyam sundar yadav <shyamkryadav345@gmail.com>
2024-02-03 12:49:12 +0545

    obj practices

---
+++ practices.js
@@ -0,0 +1,19 @@
+const prodcut = {
+  productName: "Ball pen",
+  price: 10,
+  rating: 5,
+  deal: true,
+  offer: "20%",
+};
+
+console.log(typeof prodcut);
+
+const profile = {
+  name: "shyam",
+  isFollow: false,
+  followers: 123,
+  follosing: 12,
+};
+
+console.log(typeof profile.name);
+console.log(typeof profile["isFollow"]);
