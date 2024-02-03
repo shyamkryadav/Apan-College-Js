d68336a1f9f3b2f7d4377caae19d4e0dceaf24fc
shyam sundar yadav <shyamkryadav345@gmail.com>
2024-02-03 11:01:09 +0545

    code inti

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("shyam sundar yadav");
+
+// Varaible in js
+// Varaible are a container for data
+
+// memory  allocation of variable is dynamic, it will allocate the memory as per need of data.
+var name = "Shyam Sundar Yadav"; // string type value can be
+
+console.log("shyam");
+a = null;
+b = undefined;
+console.log(typeof a);
+console.log(typeof b);
+
+isFollow = false;
+isFollow = true;
+console.log(isFollow);
+isFollow = "shyam";
+console.log(typeof isFollow);
+
+fullNmae = '554';
+
+// a and A are different value  
